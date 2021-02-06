e0373c85f65e7cffd7a46f8abe10f97dc9fca186
Ty Victorson <trv17@ymail.com>
2021-02-05 20:25:25 -0600

    card creator script

---
+++ Card.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public Sprite cardImage;
+    public string cardName;
+    public int attack;
+    public int defence;
+    public int team;
+    public int place;
+}
